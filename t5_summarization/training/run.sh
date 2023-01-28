900ba7554ff1059995ad695b3f602904d065cb9f
Jørgen Navjord <navjordj@gmail.com>
2023-01-28 22:37:47 +0100

    t5 summarization runscript

--- t5_summarization/training/run.sh
+++ t5_summarization/training/run.sh
@@ -1,5 +1,7 @@
+# https://huggingface.co/docs/transformers/main_classes/trainer
+
 python run_summarization.py \
-    --model_name_or_path t5-north/t5_small_NCC_lm \
+    --model_name_or_path north/t5_small_NCC_lm \
     --do_train \
     --train_file vg_summarization.csv \
     --text_column article \
@@ -7,6 +9,9 @@ python run_summarization.py \
     --source_prefix "oppsummer: " \
     --output_dir /tmp/tst-summarization \
     --overwrite_output_dir \
-    --per_device_train_batch_size=4 \
-    --per_device_eval_batch_size=4 \
-    --predict_with_generate
+    --per_device_train_batch_size=8 \
+    --per_device_eval_batch_size=8 \
+    --predict_with_generate \
+    --save_total_limit 2 \
+    --save_steps 5000 \
+    --num_train_epochs 2
