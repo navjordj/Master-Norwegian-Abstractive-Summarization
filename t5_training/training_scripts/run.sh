6b7ad49e8916ad23f173e76de3ffa575ae5af430
Jørgen Navjord <navjordj@gmail.com>
2023-05-13 22:38:03 +0200

    Clean up t5 training

--- t5_training/training_scripts/run.sh
+++ t5_training/training_scripts/run.sh
@@ -1,7 +1,4 @@
-import os
-
-command = """
-accelerate launch --config_file accelerate_config.yaml run_summarization.py \
+accelerate launch --config_file accelerate_config.yaml train_summarizer_transformer.py \
     --model_name_or_path north/t5_base_NCC_lm \
     --do_train \
     --do_predict \
@@ -14,7 +11,7 @@ accelerate launch --config_file accelerate_config.yaml run_summarization.py \
     --overwrite_output_dir \
     --per_device_train_batch_size=4 \
     --per_device_eval_batch_size=4 \
-    --num_train_epochs 4 \
+    --num_train_epochs 20 \
     --predict_with_generate \
     --save_total_limit 2 \
     --save_steps 25 \
@@ -23,13 +20,4 @@ accelerate launch --config_file accelerate_config.yaml run_summarization.py \
     --gradient_accumulation_steps 2 \
     --push_to_hub \
     --save_strategy epoch \
-    --load_best_model_at_end \
-    --evaluation_strategy epoch \
-    --max_train_samples 10 \
-    --max_eval_samples 20 \
-    --max_predict_samples 10
-
-"""
-
-os.system(command)
-
+    --early_stopping_patience 2
